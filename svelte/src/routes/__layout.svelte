d2510037fd241637aaf7bf991c20e84d5cb9cade
ashe <rosascript@gmail.com>
2022-08-07 05:37:24 -0400

    dark mode setup

--- svelte/src/routes/__layout.svelte
+++ svelte/src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import { classList } from 'svelte-body';
+	import { theme } from '@/stores/theme';
 </script>
 
+<svelte:body use:classList={`${$theme}`} />
 <slot />
