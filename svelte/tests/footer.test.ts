83fed6d17f3064d165dcb6e6f94b1b6fa20ce9e2
ashe <rosascript@gmail.com>
2022-08-13 16:33:29 -0400

    add code workspace

--- svelte/tests/footer.test.ts
+++ svelte/tests/footer.test.ts
@@ -2,7 +2,7 @@ import Footer from '@/components/footer.svelte';
 import { render } from '@testing-library/svelte';
 import { describe, expect, it } from 'vitest';
 
-describe('components | nav', () => {
+describe('components | footer', () => {
    it('should render', () => {
       const { container } = render(Footer);
       expect(container).toBeTruthy();
