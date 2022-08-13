2e3ccf3f4779e16014ab8fefd8850a8a34bb39d0
ashe <rosascript@gmail.com>
2022-08-13 00:09:17 -0400

    layout stuff

---
+++ svelte/src/components/page-transition.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+   import { fly } from 'svelte/transition';
+   export let url: string;
+</script>
+
+{#key url}
+   <div
+      out:fly={{ duration: 100, y: 10 }}
+      in:fly={{ delay: 100, duration: 200, y: -10 }}
+      class="m-0 h-full w-full p-0"
+   >
+      <slot />
+   </div>
+{/key}
--- svelte/src/routes/__layout.svelte
+++ svelte/src/routes/__layout.svelte
@@ -1,8 +1,23 @@
-<script>
-   import '../app.css';
-   import { classList } from 'svelte-body';
+<script lang="ts" context="module">
+   export const load: import('@sveltejs/kit').Load = async ({ url }) => ({
+      props: { url }
+   });
+</script>
+
+<script lang="ts">
+   import Footer from '@/components/footer.svelte';
+   import Nav from '@/components/nav.svelte';
+   import PageTransition from '@/components/page-transition.svelte';
    import { theme } from '@/stores/theme';
+   import { classList } from 'svelte-body';
+   import '../app.css';
+
+   export let url: string;
 </script>
 
 <svelte:body use:classList={`${$theme}`} />
-<slot />
+<Nav />
+<PageTransition {url}>
+   <slot />
+</PageTransition>
+<Footer />
