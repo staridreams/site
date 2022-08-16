554fc57141822d8829cf72d924c35d0519fc3370
ashe <rosascript@gmail.com>
2022-08-15 21:38:28 -0400

    fix responsivity issue by making underlines only appear on md and up

--- svelte/src/components/nav.svelte
+++ svelte/src/components/nav.svelte
@@ -33,7 +33,7 @@
    <ul class="max-h-16 md:flex md:items-center md:justify-between">
       {#each routes as route}
          <li
-            class="text-primary normal:duration-200 relative my-3 ml-5 cursor-pointer select-none overflow-hidden transition-all duration-300 after:absolute after:bottom-0 after:right-full after:h-[2px] after:w-16 after:rounded-lg after:bg-black after:duration-300 hover:after:translate-x-full dark:after:bg-[#b8c0e0] md:mx-14 md:my-0 md:ml-0 after:md:w-full"
+            class="text-primary normal:duration-200 relative my-3 ml-5 cursor-pointer select-none overflow-hidden transition-all duration-300 hover:font-semibold hover:italic md:mx-14 md:my-0 md:ml-0 md:after:absolute md:after:bottom-0 md:after:right-full md:after:h-[2px] md:after:w-16 md:after:md:w-full md:after:rounded-lg md:after:bg-black md:after:duration-300 hover:md:after:translate-x-full dark:md:after:bg-[#b8c0e0]"
             class:[font-weight:550]={route.path === $page.url.pathname}
          >
             <a href={route.path}>{route.name}</a>
