498b9117253c4f386fc661ccc22e2b3f6ef4dffc
ashe <rosascript@gmail.com>
2022-08-21 23:50:29 -0400

    add about page

--- svelte/src/routes/about.svelte
+++ svelte/src/routes/about.svelte
@@ -5,4 +5,14 @@
 <svelte:head>
    <title>starbunn | about</title>
 </svelte:head>
-<ContentWrapper />
+<ContentWrapper>
+   <h1 class="text-primary font-code pb-6 text-center text-2xl font-bold">
+      $ whoami
+   </h1>
+   <p class="text-primary text-center text-xl">Mᴀʀɪ</p>
+   <p class="text-primary font-code text-center italic">pronounced /mɛri/</p>
+
+   <p class="text-primary pt-6 text-center font-mono text-lg">
+      I am a software developer living in the USA.
+   </p>
+</ContentWrapper>
